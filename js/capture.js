ebb43c62f304f1f00c00cc407df99f2158dc7a02
Isabelle Nembrot <i.nembrot@laposte.net>
2013-04-08 15:26:36 +0200

    Test Photo

---
+++ js/capture.js
@@ -0,0 +1,52 @@
+	
+	var npath='';
+	
+	function sendform(){
+		var options = new FileUploadOptions();
+		options.fileKey="file";
+		options.fileName=npath.substr(npath.lastIndexOf('/')+1);
+		options.mimeType="image/jpeg";
+
+		var nomimage = Math.floor(Math.random()*15000000);
+		var ft = new FileTransfer();
+		ft.upload(npath, upload_url + '?nomimage=' + nomimage , 
+			successCallback,
+			errorCallback,
+		    options);
+	}
+	
+	function capturePhoto() {
+	    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, targetWidth:600  });
+	}
+	
+	function onFail(){
+		var msg ='Impossible de lancer l\'appareil photo';
+        navigator.notification.alert(msg, null, '');
+	}
+	
+	function onPhotoDataSuccess(imageData) {
+		// On récupère le chemin de la photo
+		alert(imageData);
+		npath = imageData.replace("file://localhost",'');
+		var path = imageData.replace("file://localhost",'');
+		alert(imageData);
+		
+		// On affiche la preview
+		$('#myImage').attr('src', path);
+		$('#myImage').show();
+		
+		// On affiche le boutton de suppression
+		$('#button_deletePhoto').show();
+	}
+	
+	function deletePhoto(){
+		$('#myImage').attr('src', '');
+		$('#myImage').hide();
+		$('#button_deletePhoto').hide();
+	}
+	
+	
+
+  
+
+	
